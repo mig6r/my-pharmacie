b048aabb4c8954625e3abeaf48b71f41d52d7fde
Sébastien Opros <s.opros@gmail.com>
2019-05-01 15:35:31 +0200

    Feature: ContactForm

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seb
+ * Date: 01/05/2019
+ * Time: 11:02
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Notification\ContactNotification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="page.contact")
+     *
+     */
+    public function show(Request $request, ContactNotification $notification)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $notification->notify($contact);
+
+            $this->addFlash('success', 'Votre Email a bien été envoyé');
+            return $this->redirectToRoute("page.contact");
+        }
+
+        return $this->render("pages/contact.html.twig", [
+            "contact" => $contact,
+            "current_menu" => "contact",
+            "form" => $form->createView()
+        ]);
+
+    }
+
+}
